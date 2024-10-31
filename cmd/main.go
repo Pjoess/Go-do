e7365cd4b3ca00b673cc2791e9e542972c613594
Pjoess <timvdkreeke@hotmail.com>
2024-10-31 17:05:09 +0100

    added a nicer display of the todo's in the terminal

--- cmd/main.go
+++ cmd/main.go
@@ -2,38 +2,30 @@ package main
 
 import (
 	// "flag"
-	// "errors"
 	"fmt"
 )
 
 func main() {
-	fmt.Println("Hello World")
-	todos := Todos{}
-	todos.add("hello todo")
-	todos.add("delete todo")
-	fmt.Printf("%+v\n", todos)
-	todos.delete(1)
-	fmt.Printf("%+v\n", todos)
-
-	err := writeJSON("../data/todos.json", todos)
-	if err != nil {
-		fmt.Println("Failed to write JSON: ", err)
-	}
-
 	data, err := readJSON("../data/todos.json")
 	if err != nil {
 		fmt.Println("Failed to read JSON: ", err)
+		return
 	}
 
-	fmt.Printf("%+v\n", data)
+	displayTodos(data)
+}
 
-	data1, err := readJSON("../data/todos.json")
-	if err != nil {
-		fmt.Println("Failed to read JSON: ", err)
+func displayTodos(todo Todos) {
+	fmt.Println("Todo list:")
+	for i := range todo {
+		status := " "
+		if todo[i].Completed {
+			status = "✓"
+		} else {
+			status = "✗"
+		}
+		fmt.Printf("[%d] %s [%s]\n", i, todo[i].Title, status)
 	}
-
-	data1.finish(0)
-	fmt.Printf("%+v\n", data1)
 }
 
 func init() {
