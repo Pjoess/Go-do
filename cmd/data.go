128489aeb754c8f09ee7d93b5e410177e80f9999
Pjoess <timvdkreeke@hotmail.com>
2024-10-31 16:30:33 +0100

    added json read and write functionality

--- cmd/data.go
+++ cmd/data.go
@@ -1,10 +1,42 @@
 package main
 
-func readTodo() []Todo{
-	// read todo's from the json file
-	return nil
+import (
+	"encoding/json"
+	"os"
+)
+
+func readJSON(filename string) (Todos, error) {
+
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	var todos Todos
+	err = json.Unmarshal(data, &todos)
+	return todos, err
+
 }
 
-func addTodoJson(){
-	// add todo to the json file 
+func writeJSON(filename string, todos Todos) error {
+	// add todo to the json file
+	data, err := json.Marshal(todos)
+	if err != nil {
+		return err
+	}
+
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = file.Write(data)
+	return err
 }
+
+// func checkFileExists(filename string) bool {
+// 	_, error := os.Stat(filePath)
+// 	//return !os.IsNotExist(err)
+// 	return !errors.Is(error, os.ErrNotExist)
+// }
--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	// "flag"
+	// "errors"
 	"fmt"
 )
 
@@ -13,6 +14,18 @@ func main() {
 	fmt.Printf("%+v\n", todos)
 	todos.delete(1)
 	fmt.Printf("%+v\n", todos)
+
+	err := writeJSON("../data/todos.json", todos)
+	if err != nil {
+		fmt.Println("Failed to write JSON: ", err)
+	}
+
+	data, err := readJSON("../data/todos.json")
+	if err != nil {
+		fmt.Println("Failed to read JSON: ", err)
+	}
+
+	fmt.Printf("%+v\n", data)
 }
 
 func init() {
